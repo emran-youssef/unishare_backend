b7a44d7acfdbaa8d7eca1106664430ca60188add
Emran Youssef Al-Khaleel <emrankhaleel03@gmail.com>
2026-04-04 00:17:11 +0300

    chore: initial project setup — Spring Boot, MySQL, Flyway, JWT, MapStruct

---
+++ src/test/java/com/unishare/unishare/UnishareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unishare.unishare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnishareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
